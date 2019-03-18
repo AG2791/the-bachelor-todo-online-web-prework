18549456b35edd4573c14e3e41f57ed85247162d
AJ Guiles <ashiguiles@gmail.com>
2019-03-18 02:01:13 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,25 +1,26 @@
 
 def get_first_name_of_season_winner(data, season)
-  data.season.each
+  
+  data[season].each
 
 end
     
     
   
-end
+# end
 
-def get_contestant_name(data, occupation)
-  # code here
-end
+# def get_contestant_name(data, occupation)
+#   # code here
+# end
 
-def count_contestants_by_hometown(data, hometown)
-  # code here
-end
+# def count_contestants_by_hometown(data, hometown)
+#   # code here
+# end
 
-def get_occupation(data, hometown)
-  # code here
-end
+# def get_occupation(data, hometown)
+#   # code here
+# end
 
-def get_average_age_for_season(data, season)
-  # code here
-end
+# def get_average_age_for_season(data, season)
+#   # code here
+# end
