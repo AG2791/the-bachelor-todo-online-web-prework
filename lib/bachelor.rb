cf2e00c1c0df0ca8daae9463ec21fef50f8b1361
AJ Guiles <ashiguiles@gmail.com>
2019-03-18 02:06:59 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,13 @@
 
 def get_first_name_of_season_winner(data, season)
   
-  data.each
+  def get_first_name_of_season_winner(data,season)
+ data[season.to_sym].each do |into_array|
+   if into_array[:status] =="Winner"
+     return into_array[:name].split(" ")[0]
+   end
+ end
+end
 
 end
     
